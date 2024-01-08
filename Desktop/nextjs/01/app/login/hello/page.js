461f58b926c6c974047cf2459c23242c213e1169
Mannan Khan <78471126+Dev-Mannan@users.noreply.github.com>
2024-01-08 21:52:06 +0530

    Delete Desktop/nextjs/01 directory
    mistakenly  pushed in Defi repo

--- Desktop/nextjs/01/app/login/hello/page.js
+++
@@ -1,7 +0,0 @@
-export default function Hello() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center font-bold p-24">
-            <h1>Hello i am from login folder name hello</h1>
-        </main>
-    )
-}
